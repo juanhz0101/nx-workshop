5fe4c78d5dcfc6e7637b8fa030abb9cda0949bec
Juan Pablo Hurtado <juan.hurtado.aliatec@gmail.com>
2022-07-28 18:02:39 -0500

    Add books feature and ui libraries

--- apps/bookstore/src/app/app.tsx
+++ apps/bookstore/src/app/app.tsx
@@ -1,16 +1,33 @@
-import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
+import React from 'react';
+import { Link, Navigate, Route, Routes } from 'react-router-dom';
+import { BooksFeature } from '@codatomic/books/feature';
+import {
+GlobalStyles,
+Header,
+Main,
+NavigationItem,
+NavigationList
+} from '@codatomic/ui';
+export const App = () => {
+return (
+<>
+<GlobalStyles />
+<Header>
+<h1>Bookstore</h1>
+<NavigationList>
+<NavigationItem>
+<Link to="/books">Books</Link>
+</NavigationItem>
+</NavigationList>
+</Header>
+<Main>
+  <Routes>
+  <Route path="/books" element={<BooksFeature />} />
+<Route  path="/" element={ <Navigate to="/books" />} />
+  </Routes>
 
-const StyledApp = styled.div`
-  // Your style here
-`;
-
-export function App() {
-  return (
-    <StyledApp>
-      <NxWelcome title="bookstore" />
-    </StyledApp>
-  );
-}
-
+</Main>
+</>
+);
+};
 export default App;
