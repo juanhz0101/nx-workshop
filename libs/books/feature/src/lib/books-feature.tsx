767fa7347322c81d53a867e5e5195e5883931768
Juan Pablo Hurtado <juan.hurtado.aliatec@gmail.com>
2022-07-28 19:56:35 -0500

    new element to affect

--- libs/books/feature/src/lib/books-feature.tsx
+++ libs/books/feature/src/lib/books-feature.tsx
@@ -1,18 +1,20 @@
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-
-/* eslint-disable-next-line */
-export interface BooksFeatureProps {}
-
-const StyledBooksFeature = styled.div`
-  color: pink;
-`;
-
-export function BooksFeature(props: BooksFeatureProps) {
-  return (
-    <StyledBooksFeature>
-      <h1>Welcome to BooksFeature!</h1>
-    </StyledBooksFeature>
-  );
-}
-
+import { getBooks, IBook } from '@codatomic/books/data-access';
+import { Books, Book } from '@codatomic/books/ui';
+export const BooksFeature = () => {
+const [books, setBooks] = useState<IBook[]>([]);
+useEffect(() => {
+ getBooks().then(setBooks)
+}, [
+// This effect runs only once on first component render
+// so we declare it as having no dependent state.
+]);
+return (
+<>
+<h2>Books</h2>
+<Books books={books} onAdd={book => alert(`Added ${book.title}`)} />
+</>
+);
+};
 export default BooksFeature;
--- libs/ui/src/lib/navigation-item/navigation-item.tsx
+++ libs/ui/src/lib/navigation-item/navigation-item.tsx
@@ -1,9 +1,12 @@
 import React, { LiHTMLAttributes } from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 const StyledNavigationItem = styled.li`
 margin-right: 1rem;
 `;
 export const NavigationItem = (props: LiHTMLAttributes<HTMLLIElement>) => {
-return <StyledNavigationItem>{props.children}</StyledNavigationItem>;
+return <StyledNavigationItem>
+    {props.children}
+    </StyledNavigationItem>;
 };
 export default NavigationItem;
